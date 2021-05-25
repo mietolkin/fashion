f7321c321589d4de9db41ca269647dc0b8cb5f67
mietolkin <82872420+mietolkin@users.noreply.github.com>
2021-05-25 16:11:33 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+
+
+  var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 5000,
+    },
+  });
+
+  var mySwiper = new Swiper('.instagram__swiper-container', {
+    containerModifierClass: 'instagram__swiper-container',
+    slidesPerView: 6,
+    spaceBetween: 1,
+    keyboard: true,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+  });
+
+
+});
